779628044cb97769d7f6e2fe733f26651684b361
asmi-g <108370938+asmi-g@users.noreply.github.com>
2022-12-26 02:57:21 -0500

    react-practice

---
+++ Footer.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+function Footer(){
+    return(
+        <footer className="footer">
+            <small>© 2023 Asmi Gujral Development. All rights reserved.</small>
+        </footer>
+    )
+}
+
+export default Footer
---
+++ MainContent.js
@@ -0,0 +1,17 @@
+import React from "react"
+
+function MainContent(){
+    return(
+        <div className="bodyText">
+            <h1>Reasons I am excited to learn React: </h1>
+            <ol>
+                <li>Job</li>
+                <li>Money</li>
+                <li>Avoid not having money</li>
+                <li>Job job job job </li>
+            </ol>
+        </div>
+    )
+}
+
+export default MainContent
